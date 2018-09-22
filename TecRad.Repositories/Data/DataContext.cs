6992a22a17f0b5bac2fa41cdb3868a8e0e1a3b11
unnsteinngardars <unnsteinng@gmail.com>
2018-09-22 11:47:45 +0000

    DataContext update

--- TecRad.Repositories/Data/DataContext.cs
+++ TecRad.Repositories/Data/DataContext.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using TecRad.Models.Author;
 using TecRad.Models.Category;
 using TecRad.Models.NewsItem;
 
@@ -14,9 +15,7 @@ namespace TecRad.Repositories.Data
 				Id = 1,
 				Title = "Trump wears a mankini",
 				ShortDescription = "Donald Trump spotted in a local swimming pool in a green Borat mankini",
-				LongDescription = "The world is shocked after a photo of American president Donald Trump wearing a "
-								+ "green borat mankini in a local swimming pool in Reykjavik, Trump supporters are "
-								+ "outraged and say that such homosexuality cannot be tolerated",
+				LongDescription = "The world is shocked after a photo of American president Donald Trump wearing a green borat mankini in a local swimming pool in Reykjavik, Trump supporters are outraged and say that such homosexuality cannot be tolerated",
 				PublishDate = DateTime.Now,
 				ModifiedBy = "Davíð Oddsson",
 				CreatedDate = DateTime.Now,
@@ -27,10 +26,7 @@ namespace TecRad.Repositories.Data
 				Id = 2,
 				Title = "Pípulagningafeðgar sekir um skattalagabrot",
 				ShortDescription = "Héraðsdómur Reykjaness hefur dæmt feðga, sem saman ráku pípulagningafyrirtæki, í fangelsi og greiðslu samtals 30 milljóna króna sektar fyrir skattalagabrot.",
-				LongDescription = "Héraðsdómur Reykjaness hefur dæmt feðga, sem saman ráku pípulagningafyrirtæki, í fangelsi og greiðslu samtals 30 milljóna króna sektar fyrir skattalagabrot."
-								+ "Faðirinn var dæmdur í átta mánaða fangelsi en sonurinn fjögurra mánaða, en fullnusta refsinga fellur niður að tveimur árum liðnum, haldi þeir almennt skilorð."
-								+ "Feðgarnir voru fundnir sekir  um að hafa ekki staðið ríkissjóði skil á virðisaukaskatti og staðgreiðslu vegna launa starfsmanna á árunum 2014 til 2016."
-								+ "Þeir neituðu báðir sök en dómurinn mat það svo að samkvæmt framburði feðganna hafi þeir báðir borið ábyrgð á rekstri fyrirtækisins. Sonurinn taldi þó að hann hafi verið notaður sem „leppur“ og að faðir hans hafi séð um að stýra fjármálum fyrirtækisins.",
+				LongDescription = "Héraðsdómur Reykjaness hefur dæmt feðga, sem saman ráku pípulagningafyrirtæki, í fangelsi og greiðslu samtals 30 milljóna króna sektar fyrir skattalagabrot. Faðirinn var dæmdur í átta mánaða fangelsi en sonurinn fjögurra mánaða",
 				PublishDate = DateTime.Now,
 				ModifiedBy = "Ásdís Erna Guðmundsdóttir",
 				CreatedDate = DateTime.Now,
@@ -41,12 +37,29 @@ namespace TecRad.Repositories.Data
 				Id = 3,
 				Title = "Jóhanna Guðrún og Davíð gengu í hjónaband við hátíðlega athöfn í Garðakirkju",
 				ShortDescription = "Tónlistarfólkið Jóhanna Guðrún Jónsdóttir og Davíð Sigurgeirsson lét pússa sig saman í Garðakirkju í Garðabæ í dag.",
-				LongDescription = "Tónlistarfólkið Jóhanna Guðrún Jónsdóttir og Davíð Sigurgeirsson lét pússa sig saman í Garðakirkju í Garðabæ í dag."
-								+ "Jóhanna Guðrún er landsfræg fyrir sönghæfileika sína og en hún hefur starfað á því sviði frá barnsaldri. Hún er hvað þekktust fyrir að hafa hafnað í öðru sæti í söngvakeppni evrópskra sjónvarpsstöðva, Eurovision, í Rússlandi árið 2009 þar sem hún flutti lagið Is it True?"
-								+ "Davíð er einn af frambærilegustu gítarleikurum landsins en hann á ekki langt að sækja hæfileikana því faðir hans er gítargoðsögnin Sigurgeir Sigmundssoni sem hefur leikið með hljómsveitum á borð við Start, Gildrunni og Drýsli.",
+				LongDescription = "Tónlistarfólkið Jóhanna Guðrún Jónsdóttir og Davíð Sigurgeirsson lét pússa sig saman í Garðakirkju í Garðabæ í dag. Jóhanna Guðrún er landsfræg fyrir sönghæfileika sína og en hún hefur starfað á því sviði frá barnsaldri.",
 				PublishDate = DateTime.Now,
 				ModifiedBy = "Unnsteinn Garðarsson",
 
+			},
+			new NewsItem
+			{
+				Id = 4,
+				Title = "Klopp vill að Liverpool verði „ljótasta“ liðið",
+				ShortDescription = "Jurgen Klopp vill að Liverpool verði „ljótasta“ liðið í ensku úrvalsdeildinni.",
+				LongDescription = "Jurgen Klopp vill að Liverpool verði „ljótasta“ liðið í ensku úrvalsdeildinni."
+								+ "Liverpool hefur spilað fótbolta sem þykir mjög skemmtilegur á að horfa undir stjórn Þjóðverjans. Með Roberto Firmino, Mohamed Salah og Sadio Mane í framlínunni er liðið með mikið og ógnandi sóknarafl.",
+				PublishDate = DateTime.Now,
+				ModifiedBy = "Ásdís Erna Guðmundsdóttir"
+			},
+			new NewsItem
+			{
+				Id = 5,
+				Title = "Siggi Jóns gerði Skagastrákana að Íslandsmeisturum",
+				ShortDescription = "Skagamenn eru komnir upp í Pepsideild karla í fótbolta á nýjan leik",
+				LongDescription = "Skagamenn eru komnir upp í Pepsideild karla í fótbolta á nýjan leik og þeir ættu að hafa nóg af ungum og flottum strákum þar sem að 2. flokkur félagsins tryggði sér Íslandsmeistaratitilinn í gær.",
+				PublishDate = DateTime.Now,
+				ModifiedBy = "Unnsteinn Garðarsson"
 			}
 
 		};
@@ -79,9 +92,85 @@ namespace TecRad.Repositories.Data
 				Name = "Erlent",
 				Slug = "erlent",
 				ParentCategoryId = 1,
-				ModifiedBy = "Unnsteinn Garðarsson"
+				ModifiedBy = "Unnsteinn Garðarsson",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now
+			},
+			new Category
+			{
+				Id = 4,
+				Name = "Veður",
+				Slug = "veður",
+				ParentCategoryId = 1,
+				ModifiedBy = "Unnsteinn Garðarsson",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now
+			},
+			new Category
+			{
+				Id = 5,
+				Name = "Íþróttir",
+				Slug = "íþróttir",
+				ParentCategoryId = 1,
+				ModifiedBy = "Unnsteinn Garðarsson",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now
+			},
+			new Category
+			{
+				Id = 6,
+				Name = "Enski Boltinn",
+				Slug = "enski-boltinn",
+				ParentCategoryId = 5,
+				ModifiedBy = "Unnsteinn Garðarsson",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now
+			},
+			new Category
+			{
+				Id = 7,
+				Name = "Íslenski boltinn",
+				Slug = "íslenski-boltinn",
+				ParentCategoryId = 5,
+				ModifiedBy = "Unnsteinn Garðarsson",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now
+			},
+			new Category
+			{
+				Id = 8,
+				Name = "Frjálsar Íþróttir",
+				Slug = "frjálsar-íþróttir",
+				ParentCategoryId = 5,
+				ModifiedBy = "Unnsteinn Garðarsson",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now
 			}
 		};
+
+		private static List<Author> _authors = new List<Author>
+		{
+			new Author
+			{
+				Id = 1,
+				Name = "Unnsteinn Garðarsson",
+				ProfileImgSource = "https://overland.org.au/wp-content/cat-on-typewriter.jpg",
+				Bio = "Unnsteinn is white cat with a black back, he is a very ambitious writer and his favorite tool is an old fashioned typewriter",
+				ModifiedBy = "Admin",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now,
+			},
+			new Author
+			{
+				Id = 2,
+				Name = "Ásdís Erna Guðmundsdóttir",
+				ProfileImgSource = "https://i.imgur.com/e0u37lt.jpg",
+				Bio = "Better known as the most serious cat in the world, she is very serious about her work as a private investigative journalist",
+				ModifiedBy = "Admin",
+				CreatedDate = DateTime.Now,
+				ModifiedDate = DateTime.Now
+			}
+		}
 
 	}
 }
